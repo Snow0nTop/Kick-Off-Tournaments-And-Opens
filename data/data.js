62947f01ea08e54ba1341cbc4928fd99caeef48b
KOTAO publisher <publisher@kotao.invalid>
2026-09-17 01:09:38 -0400

    Update results

--- data/data.js
+++ data/data.js
@@ -23,7 +23,7 @@ const RAW_DATA = {
       "b": 2,
       "bo": 3,
       "rounds": [
-        "1-2",
+        null,
         null,
         null
       ]
@@ -267,7 +267,7 @@ const RAW_DATA = {
       ]
     }
   ],
-  "generatedAt": "2026-09-17T05:09:07+00:00",
+  "generatedAt": "2026-09-17T05:09:38+00:00",
   "generatedFrom": "KOTAO_2026.xlsx"
 };
 if (typeof module !== 'undefined') module.exports = RAW_DATA;
